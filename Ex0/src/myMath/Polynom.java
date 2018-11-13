1c11ce4d38773f5c4c7842fce73ff91020d30404
meyranhezi <meyranhezi@192.168.56.1>
2018-11-13 18:57:31 +0200

    fixing root again. instead of return mid now return f(mid), and add new condition.

--- Ex0/src/myMath/Polynom.java
+++ Ex0/src/myMath/Polynom.java
@@ -227,9 +227,9 @@ public class Polynom implements Polynom_able{
 	public double root(double x0, double x1, double eps) {
 		double mid = (x0 + x1)/2;
 		if(Math.abs(f(mid)) < eps) {
-			return mid;
+			return f(mid);
 		}
-		if(mid > 0 && x0 < 0) {
+		if((mid > 0 && x0 < 0) || (mid < 0 && x0 > 0)) {
 			return root(x0, mid, eps);
 		}
 		else {
